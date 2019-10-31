a3f773d003b053ca9e942a05110a8d439f70e988
Yukio Usuzumi <anohigisavay@gmail.com>
2019-10-31 16:15:13 +0800

    Adjust code due to QMK upgrade

--- keyboards/gh60/satan/keymaps/seiwa/config.h
+++ keyboards/gh60/satan/keymaps/seiwa/config.h
@@ -1,9 +1,8 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
+#pragma once
 
 #include "../../config.h"
 
-#define BACKLIGHT_BREATHING
+// #define BACKLIGHT_BREATHING
 /* #ifdef BACKLIGHT_LEVELS */
 /*   #undef BACKLIGHT_LEVELS */
 /*   #define BACKLIGHT_LEVELS 10 */
@@ -13,5 +12,3 @@
 #define GRAVE_ESC_CTRL_OVERRIDE
 #define GRAVE_ESC_GUI_OVERRIDE
 // #define GRAVE_ESC_SHIFT_OVERRIDE
-
-#endif
